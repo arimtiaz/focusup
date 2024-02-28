e54e00b035a3f64d0f3b9948f756adf6526aa868
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2024-02-28 11:19:04 +0600

    BoilerPlate Installed

---
+++ src/pages/Popup/Popup.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../assets/img/logo.svg';
+import Greetings from '../../containers/Greetings/Greetings';
+import './Popup.css';
+
+const Popup = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Gs</h1>
+      </header>
+      <p>No Block & All</p>
+    </div>
+  );
+};
+
+export default Popup;
