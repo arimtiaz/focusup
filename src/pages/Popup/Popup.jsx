5603cbfe2f965ac990160788b311828733c82e3d
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2024-02-28 17:29:58 +0600

    Tailwind Updates

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import '../Popup/Popup.css'
 const Popup = () => {
   return (
-    <div className="App bg-slate-500">
-      <header className="App-header">
-      <h1 className='text-2xl underline font-semibold text-blue-200'>Hello World</h1>
-      <p>Na Na</p>
-      </header>
-      <div className=' bg-violet-700'></div>
+    <div className="App bg-purple-900">
+      <div>
+        <h1 className='text-2xl font-semibold text-white'>Hello World</h1>
+      </div>
     </div>
   );
 };
