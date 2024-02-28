8113e8cd1bd5f5014f1bd138c8b38f6a221714e9
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2024-02-28 17:13:52 +0600

    Tailwind Sorted

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,41 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+let activeTabID = null;
+let activeWebUrl = null;
+let startTime = null;
+
+function startTimer(tabId, url) {
+  activeTabID = tabId;
+  activeWebUrl = url;
+  startTime = new Date().getTime();
+}
+
+function stopTimer(tabId, url) {
+  if (activeTabID !== null) {
+    const endTime = new Date().getTime();
+    const elapsedTime = endTime - startTime;
+    console.log(`Time spent on ${activeWebUrl}: ${elapsedTime} milliseconds`);
+    activeTabID = null;
+    activeWebUrl = null;
+    startTime = null;
+  }
+}
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    if (tab) {
+      startTimer();
+    }
+  });
+});
+
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+  if (tabId === activeTabID) {
+    stopTimer();
+  }
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tabId === activeTabID && changeInfo.url) {
+    stopTimer();
+    startTime(tabId, changeInfo.url);
+  }
+});
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
-import './Popup.css';
-
+import '../Popup/Popup.css'
 const Popup = () => {
   return (
-    <div className="App">
+    <div className="App bg-slate-500">
       <header className="App-header">
-      <h1>Hello World</h1>
+      <h1 className='text-2xl underline font-semibold text-blue-200'>Hello World</h1>
       <p>Na Na</p>
       </header>
+      <div className=' bg-violet-700'></div>
     </div>
   );
 };
