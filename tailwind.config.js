479b51e7d920e0d83876a93b18f1bf2605985a9b
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2024-02-28 17:36:05 +0600

    Issues .1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< HEAD
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 
+=======
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}'],
+>>>>>>> secondary
   theme: {
     extend: {},
   },
